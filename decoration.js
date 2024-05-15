984e906afa5068fabbe1993c34312e92d9c4ee1c
Alexander Ivanov <hurmawe@gmail.com>
2024-05-15 21:30:19 +0300

    ver 1.0
    Firstpage, text, decoration

---
+++ decoration.js
@@ -0,0 +1,23 @@
+const leftD = document.querySelector(".leftDecoration");
+const rightD = document.querySelector(".rightDecoration");
+const textF = document.querySelector(".textFrame")
+
+let arrayDec = ["dec1.svg","dec2.svg","dec3.svg","dec4.svg","dec5.svg","dec6.svg"];
+console.log(textF.offsetHeight)
+const numberEl = +textF.offsetHeight / 162 + 1;
+
+
+for (let i = 0; i < numberEl; i++){
+    let dec = document.createElement("img");
+    let url = "res/" + arrayDec[getRandomInt(6)];
+    dec.setAttribute("src",url)
+    leftD.appendChild(dec);
+    dec = document.createElement("img");
+    url = "res/" + arrayDec[getRandomInt(6)];
+    dec.setAttribute("src",url)
+    rightD.appendChild(dec);
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
