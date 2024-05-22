f48ddc0b47fba459763bd8ca0548d6ef8c809614
Alexander Ivanov <hurmawe@gmail.com>
2024-05-23 01:06:42 +0300

    ver 1.4
    last animate

--- decoration.js
+++ decoration.js
@@ -3,7 +3,12 @@ const rightD = document.querySelector(".rightDecoration");
 const textF = document.querySelector(".textFrame")
 
 let arrayDec = ["dec1.svg","dec2.svg","dec3.svg","dec4.svg","dec5.svg","dec6.svg"];
-const numberEl =  +textF.offsetHeight / 151;
+
+let width = +leftD.offsetWidth;
+console.log(width)
+let heightEl = width * 0.65;
+
+const numberEl =  +textF.offsetHeight / 150 - 2;
 
 for (let i = 0; i < numberEl; i++){
     let dec = document.createElement("img");
